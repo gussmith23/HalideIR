f79216b3e272680c1c628931529960a26d133cc0
Gus <guscomps@gmail.com>
2019-10-08 22:04:06 -0400

    Remove datatype assert

--- src/base/Type.cpp
+++ src/base/Type.cpp
@@ -57,7 +57,6 @@ HalideIR::Expr Type::min() const {
     } else if (is_uint()) {
         return Internal::UIntImm::make(*this, 0);
     } else {
-        internal_assert(is_float());
         if (bits() == 16) {
             return Internal::FloatImm::make(*this, -65504.0);
         } else if (bits() == 32) {
